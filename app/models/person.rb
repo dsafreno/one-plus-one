6c77696308f08739260c6a9b0c3719ff47a2aee6
Douglas Safreno <dsafreno@cs.stanford.edu>
2013-10-20 05:09:15 -0700

    Proper capitalization for email and name

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,17 @@ class Person < ActiveRecord::Base
   has_and_belongs_to_many :teams
   has_many :pairings
 
+  before_validation :downcase_email
+  before_validation :titelize_name
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  private
+  def downcase_email
+    self.email = self.email.downcase
+  end
+  def titelize_name
+    self.name = self.name.titleize
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -33,4 +33,30 @@ describe Person do
     before { @person.email = @existing_person.email }
     it { should_not be_valid }
   end
+
+  describe 'when email is nonunique but uppercased' do
+    before { @person.email = @existing_person.email.upcase }
+    it { should_not be_valid }
+  end
+
+  describe 'when name is not properly titelized' do
+    before do
+      @person.name = @person.name.upcase
+      @person.valid?
+    end
+    it 'should titelize before saving' do
+      @person.name.should eq (@person.name.titleize)
+    end
+  end
+
+  describe 'when email is unique but uppercased' do
+    before do
+      @old_email = @person.email
+      @person.email = @old_email.upcase
+      @person.valid?
+    end
+    it 'should downcase before saving' do
+      @person.email.should eq(@old_email)
+    end
+  end
 end
